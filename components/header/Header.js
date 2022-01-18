013ef6ce6ca27ff632910859612c0f062f9d968e
Praveen Kumar <40033985+Praveen-kumar-nagarajan@users.noreply.github.com>
2022-01-18 20:48:12 +0530

    Netlify-React-Starter: Header mobile responsive fixed

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 export default function Header(props) {
-
   const router = useRouter();
 
   const name = "< Praveen Kumar Nagarajan />";
@@ -10,23 +10,53 @@ export default function Header(props) {
     { label: "< Projects />", nav: "/projects" },
     { label: "< Skills />", nav: "/skills" },
     { label: "< Blogs />", nav: "/blogs" },
-    { label: "< Contact />", nav: "/contact" }
+    { label: "< Contact />", nav: "/contact" },
   ];
 
   return (
-    <div className="header">
-      <a href="/about" className="owner">
-        {name}
-      </a>
-      <ul className="nav_list">
-        {navList.map(function (nav) {
-          return (
-            <li className={(nav.nav === router.pathname) ? `nav_link active` : `nav_link`} key={nav.nav}>
-              <Link href={nav.nav}>{nav.label}</Link>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <Navbar bg="light" expand="lg" className="header">
+      <Container fluid>
+        <Navbar.Brand href="/about" className="owner"> {name} </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <ul className="nav_list">
+            {navList.map(function (nav) {
+              return (
+                <li
+                  className={
+                    nav.nav === router.pathname ? `nav_link active` : `nav_link`
+                  }
+                  key={nav.nav}
+                >
+                  <Link href={nav.nav}>{nav.label}</Link>
+                </li>
+              );
+            })}
+          </ul>
+          {/* <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <Nav.Link href="/projects"> Projects </Nav.Link>
+            <Nav.Link href="/skills"> Skills </Nav.Link>
+          </Nav> */}
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
+  {
+    /* <a href="/about" className="owner">
+    {name}
+  </a>
+  <ul className="nav_list">
+    {navList.map(function (nav) {
+      return (
+        <li className={(nav.nav === router.pathname) ? `nav_link active` : `nav_link`} key={nav.nav}>
+          <Link href={nav.nav}>{nav.label}</Link>
+        </li>
+      );
+    })}
+  </ul> */
+  }
 }
